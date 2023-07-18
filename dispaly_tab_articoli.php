3b22ea5195038a2a8effb6f01b75eac7afca6c42
Rora <46594307+AuroraMaggi@users.noreply.github.com>
2023-07-18 16:53:48 +0200

    dispaly_tab_articoli.php

---
+++ dispaly_tab_articoli.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>display articoli</title>
+</head>
+<body>
+<form action='' method='post'>
+  <button type='submit' formaction="index.html" name='Indietro'>Indietro</button><br><br>
+</form>
+
+<?php
+    $myconn = mysqli_connect("localhost", "amaggi", "my_amaggi") or die("1.Errore... " . mysqli_error());
+    mysqli_select_db($myconn, "my_amaggi") or die("2.Errore... " . mysqli_error($myconn)); 
+
+    $sql = "SELECT * FROM Articoli";
+    $rs = mysqli_query($myconn, $sql) or die("3.Errore... " . mysqli_error($myconn));
+
+    $numrows = mysqli_num_rows($rs);
+    
+    if ($numrows == 0) {
+        echo "Database vuoto!";
+    } else {
+        while ($riga = mysqli_fetch_assoc($rs)) {
+            $ID = $riga["ID_A"];
+            $Nome = $riga["Nome_A"];
+            $PrezzoVendita = $riga["PrezzoVendita"];
+            $Quantita = $riga["Quantita"];
+            
+            echo "Id: <b>" . $ID . "</b>";
+            echo " - Nome: <b>" . $Nome . "</b><br/>";
+            echo "Prezzo: <b>" . $PrezzoVendita . "</b><br/>";
+        }
+    }
+    
+    mysqli_close($myconn);
+?>
+
+</body>
+</html>
